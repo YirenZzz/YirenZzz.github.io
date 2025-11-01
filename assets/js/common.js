88dbfc824a956ebd9f50abf7b1a31d617b2110b5
YirenZzz <zhaoyirendaisy@icloud.com>
2025-11-01 16:18:43 -0400

    Updated the copyright time.

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,3 +7,15 @@ $(document).ready(function() {
     });
     $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
 });
+
+// 自动更新页脚年份与日期，并保持居中
+$(function() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var dateStr = now.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+    var footerContainer = document.querySelector('footer.fixed-bottom .container.mt-0');
+    if (footerContainer) {
+        footerContainer.style.textAlign = 'center';
+        footerContainer.innerHTML = '&copy; Copyright ' + year + ' Yiren Zhao. Last updated: ' + dateStr + '.';
+    }
+});
